1c6c2e8cb780d479588f0b3d09c38f050bbad047
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-10-25 00:59:36 -0700

    WIP: snaking the LEDs to match upright position after soldering them together. Also, WIP for refactoring code quality.

--- musicalLeds/musicalLeds.ino
+++ musicalLeds/musicalLeds.ino
@@ -5,7 +5,8 @@
 // To Do: !! figure out timing for data packets for a delay
 unsigned long time;
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(59, PIN, NEO_GRB + NEO_KHZ800);
+// 1 LED was given as tribute to the soldering gods
 
 void setup() {
   // void means you will not have a return for this method
@@ -17,7 +18,6 @@ void setup() {
     ;
   }
 
-//  establishContact();  // send a byte to establish contact until receiver responds
 } // end of setup
 
 int inputArray[6];  // variable that will house the array of numbers
@@ -42,15 +42,15 @@ void loop() {
     // display pixels!
     LedSwitch();
     strip.show();
-//    delay(100);
+
   }
 
 } // end of void loop
 
 void LedSwitch() {
   // subBass
-      for(int i=0; i < 0 + 10; i++) { // from 0-9 pixel locale
-        for(int j=0; j < 0 + inputArray[0]; j++) {
+      for(int i=9; i > 0; i--) { // from 0-9 pixel locale
+        for(int j=0; j < inputArray[0]; j++) {
           strip.setPixelColor(j, 90, 190, 90);
         }
         // turn off pixels not needed
@@ -60,56 +60,56 @@ void LedSwitch() {
       } // end of first section
 
        // bass
-      for(int i=10; i < 10 + 10; i++) {
+      for(int i=9; i < 19; i++) {
         for(int j=10; j < 10 + inputArray[1]; j++) {
           strip.setPixelColor(j, 255, 255, 255);
         }
         // turn off pixels not needed
-        for(int j=10 + inputArray[1]; j < 10 + 10; j++) {
+        for(int j=10 + inputArray[1]; j < 19; j++) {
           strip.setPixelColor(j, 0);
         }
       } // end of second section
 
        // low mid-tones
-      for(int i=20; i < 20 + 10; i++) {
+      for(int i=19; i < 29; i++) {
         for(int j=20; j < 20 + inputArray[2]; j++) {
           strip.setPixelColor(j, 255, 255, 0);
         }
         // turn off pixels not needed
-        for(int j=20 + inputArray[2]; j < 20 + 10; j++) {
+        for(int j=19 + inputArray[2]; j < 29; j++) {
           strip.setPixelColor(j, 0);
         }
       }  // end of third section
 
        // mid-tones
-      for(int i=30; i < 30 + 10; i++) {
+      for(int i=29; i < 39; i++) {
         for(int j=30; j < 30 + inputArray[3]; j++) {
           strip.setPixelColor(j, 255, 150, 0);
         }
         // turn off pixels not needed
-        for(int j=30 + inputArray[3]; j < 30 + 10; j++) {
+        for(int j=29 + inputArray[3]; j < 39; j++) {
           strip.setPixelColor(j, 0);
         }
       }  // end of fourth section
 
        // high mid-tones
-      for(int i=40; i < 40 + 10; i++) {
+      for(int i=39; i < 49; i++) {
         for(int j=40; j < 40 + inputArray[4]; j++) {
           strip.setPixelColor(j, 0, 80, 100);
         }
         // turn off pixels not needed
-        for(int j=40 + inputArray[4]; j < 40 + 10; j++) {
+        for(int j=39 + inputArray[4]; j < 49; j++) {
           strip.setPixelColor(j, 0);
         }
       } // end of fifth section
 
        // treble
-      for(int i=50; i < 50 + 10; i++) {
+      for(int i=49; i < 59; i++) {
         for(int j=50; j < 50 + inputArray[5]; j++) {
           strip.setPixelColor(j, 100, 0, 100);
         }
         // turn off pixels not needed
-        for(int j=50 + inputArray[5]; j < 50 + 10; j++) {
+        for(int j=49 + inputArray[5]; j < 59; j++) {
           strip.setPixelColor(j, 0);
         }
       } // end of sixth section
