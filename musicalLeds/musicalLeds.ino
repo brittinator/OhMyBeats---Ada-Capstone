19eaf80cf916bebb9c53ee0b7747785c7ce5bf5e
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-10-25 13:18:37 -0700

    Adds right orientation for equalizer buckets after soldering in a snake-pattern.

--- musicalLeds/musicalLeds.ino
+++ musicalLeds/musicalLeds.ino
@@ -7,6 +7,7 @@ unsigned long time;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(59, PIN, NEO_GRB + NEO_KHZ800);
 // 1 LED was given as tribute to the soldering gods
+int BUCKET = 10; // size of each equalizer bar
 
 void setup() {
   // void means you will not have a return for this method
@@ -42,75 +43,74 @@ void loop() {
     // display pixels!
     LedSwitch();
     strip.show();
-
   }
-
 } // end of void loop
 
 void LedSwitch() {
   // subBass
-      for(int i=9; i > 0; i--) { // from 0-9 pixel locale
-        for(int j=0; j < inputArray[0]; j++) {
-          strip.setPixelColor(j, 90, 190, 90);
-        }
-        // turn off pixels not needed
-        for(int j=0 + inputArray[0]; j < 0 + 10; j++) {
-          strip.setPixelColor(j, 0);
-        }
-      } // end of first section
+  //array = 6
+  for(int i=9; i >= 0; i--) { // from 0-8 pixel locale
+    for(int j=9; j > inputArray[0]; j--) {
+      strip.setPixelColor(j, 90, 100, 150);
+    }
+//    // turn off pixels not needed
+//    for(int j=9 - (inputArray[0]-1); j > 0; j--) {
+//      strip.setPixelColor(j, 0);
+//    }
+  } // end of first section
 
-       // bass
-      for(int i=9; i < 19; i++) {
-        for(int j=10; j < 10 + inputArray[1]; j++) {
-          strip.setPixelColor(j, 255, 255, 255);
-        }
-        // turn off pixels not needed
-        for(int j=10 + inputArray[1]; j < 19; j++) {
-          strip.setPixelColor(j, 0);
-        }
-      } // end of second section
+   // bass
+  for(int i=9; i < 19; i++) {
+    for(int j=10; j < 10 + inputArray[1]; j++) {
+      strip.setPixelColor(j, 255, 255, 255);
+    }
+    // turn off pixels not needed
+    for(int j=10 + inputArray[1]; j < 19; j++) {
+      strip.setPixelColor(j, 0);
+    }
+  } // end of second section
 
-       // low mid-tones
-      for(int i=19; i < 29; i++) {
-        for(int j=20; j < 20 + inputArray[2]; j++) {
-          strip.setPixelColor(j, 255, 255, 0);
-        }
-        // turn off pixels not needed
-        for(int j=19 + inputArray[2]; j < 29; j++) {
-          strip.setPixelColor(j, 0);
-        }
-      }  // end of third section
+   // low mid-tones, upside down
+  for(int i=28; i > 18; i--) {
+    for(int j=28; j > 28 - inputArray[2]; j--) {
+      strip.setPixelColor(j, 200, 25, 0);
+    }
+    // turn off pixels not needed
+    for(int j=28 - inputArray[2]; j > 18; j--) {
+      strip.setPixelColor(j, 0);
+    }
+  }  // end of third section
 
-       // mid-tones
-      for(int i=29; i < 39; i++) {
-        for(int j=30; j < 30 + inputArray[3]; j++) {
-          strip.setPixelColor(j, 255, 150, 0);
-        }
-        // turn off pixels not needed
-        for(int j=29 + inputArray[3]; j < 39; j++) {
-          strip.setPixelColor(j, 0);
-        }
-      }  // end of fourth section
+   // mid-tones
+  for(int i=29; i < 39; i++) {
+    for(int j=30; j < 30 + inputArray[3]; j++) {
+      strip.setPixelColor(j, 255, 150, 0);
+    }
+    // turn off pixels not needed
+    for(int j=29 + inputArray[3]; j < 39; j++) {
+      strip.setPixelColor(j, 0);
+    }
+  }  // end of fourth section
 
-       // high mid-tones
-      for(int i=39; i < 49; i++) {
-        for(int j=40; j < 40 + inputArray[4]; j++) {
-          strip.setPixelColor(j, 0, 80, 100);
-        }
-        // turn off pixels not needed
-        for(int j=39 + inputArray[4]; j < 49; j++) {
-          strip.setPixelColor(j, 0);
-        }
-      } // end of fifth section
+   // high mid-tones, , upside down
+  for(int i=48; i > 38; i--) {
+    for(int j=48; j > 48 - inputArray[4]; j--) {
+      strip.setPixelColor(j, 0, 80, 100);
+    }
+    // turn off pixels not needed
+    for(int j=48-inputArray[4]; j > 38; j--) {
+      strip.setPixelColor(j, 0);
+    }
+  } // end of fifth section
 
-       // treble
-      for(int i=49; i < 59; i++) {
-        for(int j=50; j < 50 + inputArray[5]; j++) {
-          strip.setPixelColor(j, 100, 0, 100);
-        }
-        // turn off pixels not needed
-        for(int j=49 + inputArray[5]; j < 59; j++) {
-          strip.setPixelColor(j, 0);
-        }
-      } // end of sixth section
+   // treble
+  for(int i=49; i < 59; i++) {
+    for(int j=49; j < 50 + inputArray[5]; j++) {
+      strip.setPixelColor(j, 100, 0, 100);
+    }
+    // turn off pixels not needed
+    for(int j=49 + inputArray[5]; j < 59; j++) {
+      strip.setPixelColor(j, 0);
+    }
+  } // end of sixth section
 }
